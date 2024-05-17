983d5b863be04bc48c08184e8f02c20dcd7dbe79
cobu93 <urielcoro@outlook.com>
2024-05-16 18:17:39 -0600

    Adding architecture components

---
+++ start-localstack.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Install awslocal
+pip install awscli-local[ver1] 
+
+# Setup AWS S3
+awslocal s3api create-bucket --bucket account-manager
+
+cat > /tmp/cors-config.json << EOL
+{
+  "CORSRules": [
+    {
+      "AllowedHeaders": ["*"],
+      "AllowedMethods": ["GET", "POST", "PUT"],
+      "AllowedOrigins": ["*"],
+      "ExposeHeaders": ["ETag"]
+    }
+  ]
+}
+EOL
+
+cat /tmp/cors-config.json
+
+awslocal s3api put-bucket-cors --bucket account-manager --cors-configuration file:///tmp/cors-config.json
+
+# Create a user
+awslocal iam create-user --user-name accmgr
+
+# Create an access key
+awslocal iam create-access-key --user-name accmgr
+
+
